cce3bed1626c1727c331c34814609f3bde545058
Jeni Dang <jenipdang@yahoo.com>
2022-03-27 13:34:02 -0700

    Updated filter a tag to button

--- src/components/suggestions/Filter.js
+++ src/components/suggestions/Filter.js
@@ -4,27 +4,27 @@ const Filter = ({filterResult}) => {
   return (
     <div className='nav-scroller sticky-top py-1 mb-2' style={{background: "linear-gradient(to right, #00b4db, #0083b0)"}}>
 					<nav className='nav d-flex justify-content-between'>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('All')}>
+						<button className='btn text-white' onClick={() => filterResult('All')}>
 							All
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Landscape')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Landscape')}>
 							Landscape
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Kitchen')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Kitchen')}>
 							Kitchen
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Bedroom')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Bedroom')}>
 							Bedroom
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Appliance')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Appliance')}>
 							Appliances
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Livingroom')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Livingroom')}>
 							Livingroom
-						</a>
-						<a className='p-2 link-secondary text-white' onClick={() => filterResult('Bathroom')}>
+						</button>
+						<button className='btn text-white' onClick={() => filterResult('Bathroom')}>
 							Bathroom
-						</a>
+						</button>
 					</nav>
 				</div>
   )
